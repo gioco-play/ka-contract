a13a41f609aea0afa69b7c0ed046b48e1ace43dd
Amin56413 <Ellie.guo@goldenf.co>
2021-12-07 19:24:09 +0800

    update coding style

--- src/Contract/KAServiceInterface.php
+++ src/Contract/KAServiceInterface.php
@@ -9,39 +9,40 @@ interface KAServiceInterface {
     /**
      * 遊戲啟動
      *
-     * @param string $op_code
+     * @param string $opCode
      * @param string $account
-     * @param string $game_code
+     * @param string $gameCode
      * @param string $language
+     * @param string $backUrl
      * @return mixed
      */
-    function gameLaunch(string $op_code, string $account, string $game_code, string $language);
+    function gameLaunch(string $opCode, string $account, string $gameCode, string $language, string $backUrl = null);
 
     /**
      * 遊戲詳情
-     * @param string $op_code
+     * @param string $opCode
      * @param string $account
-     * @param string $bet_id
+     * @param string $betId
      * @return mixed
      */
-    function gameDetail(string $op_code, string $account, string $bet_id);
+    function gameDetail(string $opCode, string $account, string $betId);
 
     /**
      * 抓取遊戲紀錄
      *
-     * @param string $op_code
-     * @param integer $past_minutes
-     * @param string $cache_key
+     * @param string $opCode
+     * @param integer $pastMinutes
+     * @param string $cacheKey
      * @return mixed
      */
-    function betLogGrabber(string $op_code, int $past_minutes, string $cache_key);
+    function betLogGrabber(string $opCode, int $pastMinutes, string $cacheKey);
 
     /**
      * 全營商 抓取遊戲紀錄
      *
-     * @param integer $past_minutes
+     * @param integer $pastMinutes
      * @return mixed
      */
-    function betLogGrabberAll(int $past_minutes);
+    function betLogGrabberAll(int $pastMinutes);
 }
 
